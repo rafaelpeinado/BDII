d99930e89ce123587c55beb821e1d27440bb409f
rafaelpeinado <rafael.peinado.silva@gmail.com>
2019-03-21 22:25:50 -0300

    20190321

--- modelo_er.sql
+++ modelo_er.sql
@@ -57,3 +57,34 @@ CREATE TABLE relTurmaDisciplina (
 ) ENGINE = InnoDB;
 
 
+INSERT INTO tblestudante VALUES (NULL, "SP1234567", "RAFAEL SILVA", "1997-03-28");
+INSERT INTO tblestudante VALUES (NULL, "SP9876543", "AMANDA SOUZA", "1999-09-04");
+INSERT INTO tblestudante VALUES (NULL, "SP1237896", "JOSÉ ANTÔNIO", "1980-05-09");
+
+INSERT INTO tbldocente VALUES (NULL, "SP4567893", "OSCAR OLIVEIRA", "1972-06-21", "MESTRADO");
+INSERT INTO tbldocente VALUES (NULL, "SP8529637", "MARIA APARECIDA", "1965-06-24", "DOUTORADO");
+INSERT INTO tbldocente VALUES (NULL, "SP1472583", "SILVANA PEREIRA", "1980-06-29", "GRADUADO");
+
+INSERT INTO tbldisciplina VALUES (NULL, "DB2A3", "GRADUAÇÃO");
+
+INSERT INTO tblturma VALUES (NULL, "2019-03-01", "NOTURNO");
+
+INSERT INTO relDocenteDisciplina VALUES (NULL, 1, 1);
+INSERT INTO relDocenteDisciplina VALUES (NULL, 2, 1);
+INSERT INTO relDocenteDisciplina VALUES (NULL, 3, 1);
+
+INSERT INTO relEstudanteTurma VALUES (NULL, 1, 1);
+INSERT INTO relEstudanteTurma VALUES (NULL, 2, 1);
+INSERT INTO relEstudanteTurma VALUES (NULL, 3, 1);
+
+INSERT INTO relTurmaDisciplina VALUES (NULL, 1, 1);
+
+-- 1º adicionar upk "NULL"
+-- 2º UPDATE 
+-- 3º mudar upk para NOT NULL
+-- em 4 entidades
+
+-- ALTER TABLE tblestudante ALTER CONSTRAINTS pkEstudante INTEGER NOT NULL;
+
+--ALTER TABLE tblestudante DROP PRIMARY KEY;
+ALTER TABLE tblestudante ADD COLUMN upkEstudante BINARY(16) NULL FIRST; 
