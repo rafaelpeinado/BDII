316ba6c8281eece928f4b43b0ac59ef2d58d4ea9
rafaelpeinado <rafael.peinado.silva@gmail.com>
2019-04-04 22:18:00 -0300

    aula04042019

--- modelo_er.sql
+++ modelo_er.sql
@@ -114,7 +114,7 @@ CREATE TABLE tblestudante (
   upkEstudante 		BINARY(16) NOT NULL,
   dcCreated 		DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dcModified 		DATETIME NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  strProntuario 	VARCHAR(7) NOT NULL UNIQUE,
+  strProntuario 	CHAR(7) NOT NULL UNIQUE,
   strNome 			VARCHAR(50) NOT NULL,
   dtNascimento 		DATE NOT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8;
@@ -124,10 +124,10 @@ CREATE TABLE tbldocente (
   upkDocente 		BINARY(16) NOT NULL,
   dcCreated 		DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dcModified 		DATETIME NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  strProntuario 	VARCHAR(7) NOT NULL UNIQUE,
+  strProntuario 	CHAR(7) NOT NULL UNIQUE,
   strNome 			VARCHAR(50) NOT NULL,
   dtNascimento 		DATE NOT NULL,
-  strEscolaridade 	VARCHAR(200) NOT NULL
+  strEscolaridade 	ENUM('Ensino Fundamental', 'Ensino Médio', 'Tecnologia', 'Bacharelado', 'Licenciatura', 'Especialização', 'Mestrado', 'Doutorado', 'Livre Docência')
 ) ENGINE = InnoDB CHARACTER SET = utf8;
 
 CREATE TABLE tblturma (
@@ -152,42 +152,42 @@ CREATE TABLE tbldisciplina (
 CREATE FUNCTION fnNovaChave() RETURNS BINARY(16) DETERMINISTIC
    RETURN UNHEX(REPLACE(UUID(), '-', ''));
    
-INSERT INTO tblestudante(upkEstudante, strProntuario, strNome, dtNascimento) VALUES (fnNovaChave(), "SP1234567", "RAFAEL SILVA", "1997-03-28");
-INSERT INTO tblestudante(upkEstudante, strProntuario, strNome, dtNascimento) VALUES (fnNovaChave(), "SP9876543", "AMANDA SOUZA", "1999-09-04");
-INSERT INTO tblestudante(upkEstudante, strProntuario, strNome, dtNascimento) VALUES (fnNovaChave(), "SP1237896", "JOSÉ ANTÔNIO", "1980-05-09");
+INSERT INTO tblestudante(upkEstudante, strProntuario, strNome, dtNascimento) VALUES (fnNovaChave(), "1234567", "RAFAEL SILVA", "1997-03-28");
+INSERT INTO tblestudante(upkEstudante, strProntuario, strNome, dtNascimento) VALUES (fnNovaChave(), "9876543", "AMANDA SOUZA", "1999-09-04");
+INSERT INTO tblestudante(upkEstudante, strProntuario, strNome, dtNascimento) VALUES (fnNovaChave(), "1237896", "JOSÉ ANTÔNIO", "1980-05-09");
 
-INSERT INTO tbldocente(upkDocente, strProntuario, strNome, dtNascimento, strEscolaridade) VALUES (fnNovaChave(), "SP4567893", "OSCAR OLIVEIRA", "1972-06-21", "MESTRADO");
-INSERT INTO tbldocente(upkDocente, strProntuario, strNome, dtNascimento, strEscolaridade) VALUES (fnNovaChave(), "SP8529637", "MARIA APARECIDA", "1965-06-24", "DOUTORADO");
-INSERT INTO tbldocente(upkDocente, strProntuario, strNome, dtNascimento, strEscolaridade) VALUES (fnNovaChave(), "SP1472583", "SILVANA PEREIRA", "1980-06-29", "GRADUADO");
+INSERT INTO tbldocente(upkDocente, strProntuario, strNome, dtNascimento, strEscolaridade) VALUES (fnNovaChave(), "4567893", "OSCAR OLIVEIRA", "1972-06-21", "MESTRADO");
+INSERT INTO tbldocente(upkDocente, strProntuario, strNome, dtNascimento, strEscolaridade) VALUES (fnNovaChave(), "8529637", "MARIA APARECIDA", "1965-06-24", "DOUTORADO");
+INSERT INTO tbldocente(upkDocente, strProntuario, strNome, dtNascimento, strEscolaridade) VALUES (fnNovaChave(), "1472583", "SILVANA PEREIRA", "1980-06-29", "Especialização");
 
 INSERT INTO tblturma(upkTurma, dtDia, strPeriodo) VALUES (fnNovaChave(), "2019-03-01", "NOTURNO");
 
 INSERT INTO tbldisciplina(upkDisciplina, strSigla, fkTurma) VALUES (fnNovaChave(), "DB2A3", 1);
 
-DELIMITER //
-CREATE PROCEDURE prCadastrarDocente (varProntuario VARCHAR(7), varNome VARCHAR(50), varEscolaridade VARCHAR(200), varNascimento DATE)
+/*DELIMITER //
+CREATE PROCEDURE prCadastrarDocente (varProntuario CHAR(7), varNome VARCHAR(50), varEscolaridade VARCHAR(200), varNascimento DATE)
 	 BEGIN
 		 START TRANSACTION; -- se tiver só uma query, é preciosismo
 			 INSERT INTO tbldocente(upkDocente, strProntuario, strNome, strEscolaridade, dtNascimento) VALUES (fnNovaChave(), varProntuario, varNome, varEscolaridade, varNascimento);
 		 COMMIT;
 	 END//
-DELIMITER ;	 
+DELIMITER ;	
 
-CALL prCadastrarDocente ("SP1234567", "ANA MARIA BRAGA", "SUPERIOR", "1923-10-12");
+CALL prCadastrarDocente ("1234567", "ANA MARIA BRAGA", "SUPERIOR", "1923-10-12");*/
 
 DELIMITER //
-CREATE PROCEDURE prCriarEstudante(varProntuario VARCHAR(7), varNome VARCHAR(50), varNascimento DATE)
+CREATE PROCEDURE prCriarEstudante(varProntuario CHAR(7), varNome VARCHAR(50), varNascimento DATE)
   BEGIN
     START TRANSACTION;
-	  INSERT INTO tblestudante(upkEstudante, strProntuario, strNome, dtNascimento) VALUES (fnNovaChave(), varProntuario, varNome, dtNascimento);
+	  INSERT INTO tblestudante(upkEstudante, strProntuario, strNome, dtNascimento) VALUES (fnNovaChave(), varProntuario, varNome, varNascimento);
 	COMMIT;
   END//
 DELIMITER ;
 
-CALL prCriarEstudante("SP3216547", "ANTONIO VIEIRA", "1968-08-27");
+CALL prCriarEstudante("3216547", "ANTONIO VIEIRA", "1968-08-27");
 
 DELIMITER //
-CREATE PROCEDURE prAtualizarEstudante(varProntuario VARCHAR(7), varNome VARCHAR(50), varNascimento DATE)
+CREATE PROCEDURE prAtualizarEstudante(varProntuario CHAR(7), varNome VARCHAR(50), varNascimento DATE)
   BEGIN
     START TRANSACTION;
 	  UPDATE tblestudante SET strNome = varNome, dtNascimento = varNascimento WHERE strProntuario = varProntuario;
@@ -195,10 +195,10 @@ CREATE PROCEDURE prAtualizarEstudante(varProntuario VARCHAR(7), varNome VARCHAR(
   END//
 DELIMITER ;
 
-CALL prAtualizarEstudante("SP3216547", "ANTONIO VIEIRA DA SILVA", "1968-08-27");
+CALL prAtualizarEstudante("3216547", "ANTONIO VIEIRA DA SILVA", "1968-08-27");
 
 DELIMITER //
-CREATE PROCEDURE prLerEstudante(varProntuario VARCHAR(7))
+CREATE PROCEDURE prLerEstudante(varProntuario CHAR(7))
   BEGIN
     START TRANSACTION;
 	  SELECT e.dcCreated, e.dcModified, e.strProntuario, e.strNome, e.dtNascimento FROM tblestudante e WHERE e.strProntuario = varProntuario;
@@ -206,17 +206,71 @@ CREATE PROCEDURE prLerEstudante(varProntuario VARCHAR(7))
   END//
 DELIMITER ;
 
-CALL prLerEstudante("SP3216547");
+CALL prLerEstudante("3216547");
 
 DELIMITER //
-CREATE PROCEDURE prDeletarEstudante(varProntuario VARCHAR(7))
+CREATE PROCEDURE prDeletarEstudante(varProntuario CHAR(7))
   BEGIN
     START TRANSACTION;
-	  DELETE FROM tblestudante e WHERE e.strProntuario = varProntuario;
+	  DELETE FROM tblestudante WHERE strProntuario = varProntuario;
 	COMMIT;
   END//
 DELIMITER ;
 
-CALL prDeletarEstudante("SP3216547");
+CALL prDeletarEstudante("3216547");
 
+DELIMITER //
+CREATE PROCEDURE prListarEstudante()
+  BEGIN
+    START TRANSACTION;
+	  SELECT e.dcCreated, e.dcModified, e.strProntuario, e.strNome, e.dtNascimento FROM tblestudante e;
+	COMMIT;
+  END//
+DELIMITER ;
+
+CALL prListarEstudante();
+
+DELIMITER //
+CREATE PROCEDURE prCriarDocente(varProntuario CHAR(7), varNome VARCHAR(50), varNascimento DATE, varEscolaridade ENUM('Ensino Fundamental', 'Ensino Médio', 'Tecnologia', 'Bacharelado', 'Licenciatura', 'Especialização', 'Mestrado', 'Doutorado', 'Livre Docência'))
+  BEGIN
+    START TRANSACTION;
+	  INSERT INTO tbldocente(upkDocente, strProntuario, strNome, dtNascimento, strEscolaridade) VALUES (fnNovaChave(), varProntuario, varNome, varNascimento, varEscolaridade);
+	COMMIT;
+  END//
+DELIMITER ;
+
+CALL prCriarDocente("9872243", "THIAGO XAVIER", "1992-03-10", "Bacharelado");
+
+DELIMITER //
+CREATE PROCEDURE prAtualizarDocente(varProntuario CHAR(7), varNome VARCHAR(50), varNascimento DATE, varEscolaridade ENUM('Ensino Fundamental', 'Ensino Médio', 'Tecnologia', 'Bacharelado', 'Licenciatura', 'Especialização', 'Mestrado', 'Doutorado', 'Livre Docência'))
+  BEGIN
+    START TRANSACTION;
+	  UPDATE tbldocente SET strNome = varNome, dtNascimento = varNascimento, strEscolaridade = varEscolaridade WHERE strProntuario = varProntuario;
+	COMMIT;
+  END//
+DELIMITER ;
+
+CALL prAtualizarDocente("9872243", "THIAGO XAVIER DA SILVA", "1992-03-10", "Bacharelado");
+
+DELIMITER //
+CREATE PROCEDURE prDeletarDocente(varProntuario CHAR(7))
+  BEGIN
+    START TRANSACTION;
+	  DELETE FROM tbldocente WHERE strProntuario = varProntuario;
+	COMMIT;
+  END//
+DELIMITER ;
+
+CALL prDeletarDocente("4567893");
+
+DELIMITER //
+CREATE PROCEDURE prListarDocente()
+  BEGIN
+    START TRANSACTION;
+	  SELECT d.dcCreated, d.dcModified, d.strProntuario, d.strNome, d.strEscolaridade, d.dtNascimento FROM tbldocente d;
+	COMMIT;
+  END//
+DELIMITER ;
+
+CALL prListarDocente();
 
